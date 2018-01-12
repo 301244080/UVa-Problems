1557a9ba8b43d0e57d07c8900c886d1fd46b3058
unknown <aiouquan@hotmail.com>
2018-01-11 20:54:19 -0800

    add file read and test.txt file

--- Assignment1_3/src/question3.java
+++ Assignment1_3/src/question3.java
@@ -1,11 +1,19 @@
 /*
  * Question 3
  */
+import java.io.FileReader; 
+import java.io.*;
+import java.io.IOException;
 public class question3 {
 
-	public static void main(String[] args) {
-		System.out.println("asdas");
-
+	public static void main(String[] args) throws IOException {
+		FileReader testFile=new FileReader("test.txt");    
+        char [] input = new char[50]; 
+        testFile.read(input);
+        //System.out.print(input[2]);    
+        for(char c : input)
+            System.out.print(c);
+        testFile.close();
 	}
 
 }
