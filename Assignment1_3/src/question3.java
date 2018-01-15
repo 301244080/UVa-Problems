fce8f2e9367abc0e944d6e8bea05acb68937090f
unknown <aiouquan@hotmail.com>
2018-01-14 22:49:06 -0800

    fixed the dfs with visited array

--- Assignment1_3/src/question3.java
+++ Assignment1_3/src/question3.java
@@ -12,32 +12,82 @@ static int path[]= new int[5005];
 static int visited[]=new int[5005];
 static int length;
 
-	static boolean dfs(int left, int right, int fa)
+//	static boolean dfs(int left, int right, int fa)
+//	{
+//		visited[left]=true;
+//		path[length++]=left;
+//		for(int a=0;a<10;a++){
+//			System.out.print(path[a]);
+//			
+//		}
+//		System.out.println("end");
+//		if(left==right)
+//		{
+//			return true;
+//		}
+//		
+//		for(int i=0; i<adjList[left].size();i++)
+//		{
+//			int a = adjList[left].get(i);
+//			if(visited[a])
+//			{
+//				continue;
+//			}
+//			
+//			if(dfs(a,right,left))
+//			{
+//				
+//				return true;
+//			}
+//		}
+//		
+//		return false;
+//		
+//	}
+	static boolean dfs(int left, int right, int distance)
 	{
 		
-		path[length++]=left;
-		for(int a=0;a<10;a++){
-			System.out.print(path[a]);
-			
-		}
-		System.out.println("end");
-		if(left==right)
+		if(right==left)
 		{
+			if(distance%2==0)
+			{
+				int index=right;
+				for(int i=0;i<distance/2;i++)
+				{
+					index=path[index];
+				}
+				System.out.println("The fleas meet at "+ index);
+			}
+			else
+			{
+				int index=right;
+				int a;
+				for(int i=0;i<(distance-1)/2;i++)
+				{
+					index=path[index];
+				}
+				a=path[index];
+				if(index>a)
+					{
+						int temp=index;
+						index=a;
+						a=temp;
+					}
+					System.out.println("The fleas jump forever between "+index+" and "+a);
+			}
 			return true;
 		}
-		
-		for(int i=0; i<adjList[left].size();i++)
+		for(int i=0;i<adjList[left].size();i++)
 		{
-			int a = adjList[left].get(i);
-			if(fa==a)
+			int b = adjList[left].get(i);
+			if(visited[b]==0)
 			{
-				continue;
-			}
-			
-			if(dfs(a,right,left))
-			{
-				
-				return true;
+				visited[b]=1;
+				path[b]=left;
+				if(dfs(b,right,distance+1))
+				{
+					return true;
+				}
 			}
 		}
 		
@@ -86,26 +136,29 @@ static int length;
 				int leftInt2 = Integer.parseInt(st.nextToken());
 				int rightInt2 = Integer.parseInt(st.nextToken());
 				length=0;
-				Arrays.fill(path, -1);
-				dfs(leftInt2,rightInt2,-1);
+				Arrays.fill(path, 0);
+				Arrays.fill(visited, 0);
+				//dfs(leftInt2,rightInt2,-1);
+				visited[leftInt2]=1;
+				dfs(leftInt2,rightInt2,0);
 				
-				if(length%2!=0)
-				{
-					System.out.println("The fleas meet at "+ path[length/2]);
-					
-				}
-				else
-				{
-					int a= path[length/2-1];
-					int b= path[length/2];
-					if(a>b)
-					{
-						int temp=a;
-						a=b;
-						b=temp;
-					}
-					System.out.println("The fleas jump forever between "+a+" and "+b);
-				}
+//				if(length%2!=0)
+//				{
+//					System.out.println("The fleas meet at "+ path[length/2]);
+//					
+//				}
+//				else
+//				{
+//					int a= path[length/2-1];
+//					int b= path[length/2];
+//					if(a>b)
+//					{
+//						int temp=a;
+//						a=b;
+//						b=temp;
+//					}
+//					System.out.println("The fleas jump forever between "+a+" and "+b);
+//				}
 			}
 			numberOfLoops++;
 			
