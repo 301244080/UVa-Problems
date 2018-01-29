d2b55fd57d90b8e65ddd54139f8e2f38740bffd4
Alex Li <li1995717@gmail.com>
2018-01-28 16:31:52 -0800

    add Assignment2 GATTACA

---
+++ A2/GATTACA/GATTACA.java
@@ -0,0 +1,88 @@
+import java.util.*;
+import java.io.*;
+
+
+
+public class GATTACA{
+
+    static final String letterTable = "ACGT";
+
+    /*
+    GATTACA
+    ATTACA
+    TTACA
+    TACA
+    ACA
+    CA
+    A
+
+    After sort:
+
+    A
+    ACA
+    ATTACA
+    CA
+    GATTACA
+    TACA
+    TTACA
+
+    */
+    public static String lcp(String str1, String str2){
+        int lenSize = Math.min(str1.length(),str2.length());
+        for (int i = 0; i < lenSize; i++) {
+            if (str1.charAt(i) != str2.charAt(i))
+                return str1.substring(0, i);
+        }
+        return str1.substring(0,lenSize);
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        int inputSize=Integer.parseInt(br.readLine());
+        for(int i=0; i<inputSize; i++){
+            String str = br.readLine();
+            int strLen = str.length();
+            String[] suffixStr = new String[strLen];
+            String ans = "";
+            int times = 1;
+
+            for(int j=0;j<strLen;j++){
+                suffixStr[j] = str.substring(j,strLen);
+            }
+
+            Arrays.sort(suffixStr);
+
+            // ORG: GATTACA
+            // A
+            // ACA
+            // ATTACA
+            // CA
+            // GATTACA
+            // TACA
+            // TTACA
+
+            for(int j=0;j<strLen;j++){
+                System.out.println(suffixStr[j]);
+            }
+
+
+            for(int k=0;k<strLen-1;k++){
+                String currRepeat =  lcp(suffixStr[k],suffixStr[k+1]);
+
+                if(currRepeat.length() > ans.length()){
+                    ans = currRepeat;
+                    times = 1;
+                }
+                if(currRepeat.equals(ans)){
+                    times++;
+                }
+            }
+
+
+            String res = ans.equals("") ? ("No repetitions found!") : (ans + " "+ times);
+            System.out.println(res);
+        }
+    }
+
+}
